e14829f7f975349b117409a2900b9dc95a55a2a1
keschaefer <keschaefer@gmail.com>
2019-01-23 10:50:03 -0700

    further styling on Sign In view, added content to Sign Up component

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,49 +1,67 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput, Button, Image } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Button, Image, ImageBackground } from 'react-native';
 import Logo from '../assets/SunUp_Logo.png';
+import BackgroundImg from '../assets/solar-panels.jpg'
 
 const SignIn = (props) => {
    return(
-      <View style= {styles.inputContainer}>
-         <Image style= {styles.image} source= {Logo} />
-         <TextInput style={styles.inputPlace}
-            placeholder= "email"/>
-         <TextInput style={styles.inputPlace}
-            placeholder= "password"/>
-            {/* onChangeText= {props.placeNameChangeHandler} */}
-         <View style= {styles.buttonContainer}>
-            <Button style={styles.inputButton} title= "Sign In" />
-            <Button title= "Sign Up"/>
-         </View>
+     <View style= {styles.background}>
+         <ImageBackground source={BackgroundImg} style={{width: 'auto', height: '100%'}}>
+            <View style= {styles.inputContainer}>
+               <Image style= {styles.image} source= {Logo} />
+               <TextInput style={styles.inputUser}
+                  placeholder= "email"/>
+               <TextInput style={styles.inputUser}
+                  placeholder= "password"/>
+                  {/* onChangeText= {props.placeNameChangeHandler} */}
+               <View style= {styles.buttonContainer}>
+                  <View style={styles.inputButton}>
+                     <Button color='white' title= "Sign In"/>
+                  </View>
+                  <View style={styles.inputButton}>
+                     <Button color='white'title= "Sign Up"/>
+                  </View>
+               </View>
+            </View>
+         </ImageBackground>
       </View>
    )
 }
 
 const styles = StyleSheet.create({
+   background: {
+      opacity: 20
+   },
    inputContainer: {
       flexDirection: 'column',
       justifyContent: 'space-around',
       alignItems: 'center',
       height: "90%"
     },
-    inputPlace: {
+    inputUser: {
       width: "70%",
       borderWidth: 1,
       borderColor: '#A0A0A0',
       height: 30,
-      borderRadius: 3
+      borderRadius: 3,
+      backgroundColor: 'white'
     },
     image: {
       width: 225,
       height: 225,
       marginTop: 50,
+      opacity: 50
     },
     inputButton: {
-      backgroundColor: 'red'
+      minWidth: 50,
+      backgroundColor: '#0098F7',
+      margin: 15,
+      borderRadius: 5,
     },
     buttonContainer: {
+      display: "flex",
       flexDirection: 'row',
-      justifyContent: 'space-around'
+      alignContent: 'space-between'
     }
     
 })
---
+++ src/Components/SignUp.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity, TextInput, Button, Image, ImageBackground } from 'react-native';
+import Logo from '../assets/SunUp_Logo.png';
+import BackgroundImg from '../assets/solar-panels.jpg'
+
+const SignUp = (props) => {
+   return(
+     <View>
+         <ImageBackground source={BackgroundImg} style={{width: 'auto', height: '100%'}}>
+            <View style= {styles.inputContainer}>
+               <Image style= {styles.image} source= {Logo} />
+               <TextInput style={styles.inputUser}
+                  placeholder= "Full Name"/>
+               <TextInput style= {styles.inputUser}
+                  placeholder= "Company Name"/>
+               <TextInput style={styles.inputUser}
+                  placeholder= "Email"/>
+               <TextInput style={styles.inputUser}
+               placeholder= "Password"/>
+               <TextInput style={styles.inputUser}
+               placeholder= "Confirm Password"/>
+                  {/* onChangeText= {props.placeNameChangeHandler} */}
+               <View style= {styles.buttonContainer}>
+                  <View style={styles.inputButton}>
+                     <Button color='white' title= "Existing User?"/>
+                  </View>
+                  <View style={styles.inputButton}>
+                     <Button color='white'title= "Sign Up"/>
+                  </View>
+               </View>
+            </View>
+         </ImageBackground>
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   inputContainer: {
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      height: "90%",
+    },
+    inputUser: {
+      width: "70%",
+      borderWidth: 1,
+      borderColor: '#A0A0A0',
+      height: 30,
+      borderRadius: 3,
+      backgroundColor: 'white'
+    },
+    image: {
+      width: 225,
+      height: 225,
+      marginTop: 50,
+      opacity: 80
+    },
+    inputButton: {
+      minWidth: 50,
+      backgroundColor: '#0098F7',
+      margin: 15,
+      borderRadius: 5,
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around'
+    }
+    
+})
+
+export default SignUp
