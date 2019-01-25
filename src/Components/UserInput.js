2b03c7e89aec3118c6d81c6f38f84a360fc93d4a
keschaefer <keschaefer@gmail.com>
2019-01-25 15:07:10 -0700

    added a financial inputs mutation, not functional yet

--- src/Components/UserInput.js
+++ src/Components/UserInput.js
@@ -1,84 +1,81 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, TouchableOpacity, TextInput, Button, Image, ImageBackground, Text } from 'react-native';
-import { Actions } from 'react-native-router-flux'
-import BackgroundImg from '../assets/solar-panels.jpg'
-import ApolloClient from 'apollo-boost';
-import { gql } from 'apollo-boost'
-import { graphql } from 'react-apollo'
+import React, { Component } from "react";
+import { View, StyleSheet, TextInput, Button, Text } from "react-native";
+import { Actions } from "react-native-router-flux"
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag"
+import { graphql, compose } from "react-apollo"
 
-let users = []
+// const client = new ApolloClient({
+//    uri: "http://sun-up-back.herokuapp.com/graphql"
+// });
 
-const client = new ApolloClient({
-   uri: 'http://sun-up-back.herokuapp.com/graphql'
- });
+const getUser = gql`
+query {
+	user(id: "5c4b2e95769ad66ee19f2f88"){
+      name_full
+      org_name
+	}
+}
+`
+const addUserFinancialInputs = gql`
+mutation {
+   addUser(password: "", name_full: "", email: "", org_name: ""){
+      id
+      name_full
+   }
+}
+`
 
-client
- .query({
-    query: gql`
-       {
-       users {
-          name_full
-       }
-       }
-    `
- }).then(result => console.log(result.data.users))
-//  .then(result => {
-//     this.setState({
-//        users: result
-//     })
-//  }   
-
-
-  class UserInput extends Component {
-   constructor() {
-      super() 
-         this.state = {
-            users: []
-         }
+class UserInput extends Component {
+constructor() {
+   super() 
+      this.state = {
       }
+   }
 
-   render() {
+render() {
+   if (!this.props.getUser.loading) {
       return(
-         <View>
-            <ImageBackground source={BackgroundImg} style={{width: 'auto', height: '100%'}}>
-               <View style= {styles.inputContainer}>
-                  <Text style={{color:'white'}} >We need a little info to estimate to your energy outlook</Text>
-                  <TextInput style={styles.inputUser} 
-                     placeholder= "Company's estimated taxes 2019"/>
-                     <Text style={{color:'white'}} >*Your 2018 tax burden is good placeholder if you don't anticipate much change in your taxes for 2019!</Text>
-                  <TextInput style= {styles.inputUser}
-                     placeholder= "Company's estimated annual energy cost 2019"/>
-                     <Text style={{color:'white'}}>*Your 2018 energy bill is good placeholder if you don't anticipate much change in your energy needs for 2019!</Text>
-                  <TextInput style={styles.inputUser}
-                     placeholder= "Amount of annual energy generated in kW"/>
-                     <Text style={{color:'white'}}>*Visit www.projectsunroof.com to estimate your energy potential</Text>
-                  <View style= {styles.buttonContainer}>
-                     <View style={styles.inputButton}>
-                        <Button color='white'title= "Submit" onPress={() => Actions.useroutlook()}/>
-                     </View>
-                  </View>
+         <View style= {styles.inputContainer}>
+            <Text style = {styles.header}>{this.props.getUser.user.name_full}, we need a little info to estimate to your energy outlook</Text>
+               <TextInput style={styles.inputUser} 
+               placeholder= "2019 Taxes"/>
+               <Text style = {styles.subText}>*Your 2018 tax burden is good placeholder if you don"t anticipate much change in your taxes for 2019</Text>
+               <TextInput style= {styles.inputUser}
+               placeholder= "2019 Energy Bill"/>
+               <Text style={styles.subText}>*Your 2018 energy bill is good placeholder if you don"t anticipate much change in your energy needs for 2019</Text>
+               <TextInput style={styles.inputUser}
+               placeholder= "kW Generated Annually"/>
+               <Text style={styles.subText}>*Visit www.projectsunroof.com to estimate your energy potential</Text>
+               <View style={styles.inputButton}>
+                  <Button color="white"title= "Submit" onPress={() => Actions.useroutlook()}/>
                </View>
-            </ImageBackground>
          </View>
-      )
+      )} else {
+         return (
+            <View>
+               <Text>Loading...</Text>
+            </View>
+         )
+      }
    }
 }
 
 const styles = StyleSheet.create({
    inputContainer: {
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      height: "90%",
-      padding: 20
+      flexDirection: "column",
+      justifyContent: "space-around",
+      height: "100%",
+      padding: 20,
+      backgroundColor: "white"
     },
     inputUser: {
       width: "70%",
       borderWidth: 1,
-      borderColor: '#A0A0A0',
+      borderColor: "#A0A0A0",
       height: 30,
       borderRadius: 3,
-      backgroundColor: 'white'
+      backgroundColor: "white"
     },
     image: {
       width: 225,
@@ -88,15 +85,25 @@ const styles = StyleSheet.create({
     },
     inputButton: {
       minWidth: 50,
-      backgroundColor: '#0098F7',
+      backgroundColor: "#0098F7",
       margin: 8,
       borderRadius: 5,
     },
     buttonContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-around'
+      flexDirection: "row",
+      justifyContent: "space-around"
+    },
+    header: {
+      fontWeight: "bold",
+      fontSize: 20
+    },
+    subText: {
+      fontSize: 10
     }
     
 })
 
-export default UserInput
+export default compose(
+   graphql(addUserFinancialInputs, {name: "addUserFinancialInputs"}),
+   graphql(getUser, {name: "getUser"})
+)(UserInput)
