0b808fa3a1491feb99ea164b8f98b826520e4ce3
keschaefer <keschaefer@gmail.com>
2019-01-26 17:39:48 -0700

    added MatchMaker component to router for UserOutlook

--- Router.js
+++ Router.js
@@ -4,6 +4,7 @@ import SignIn from './src/Components/SignIn.js';
 import SignUp from './src/Components/SignUp.js';
 import UserInput from './src/Components/UserInput.js';
 import UserOutlook from './src/Components/UserOutlook.js';
+import MatchMakingOutput from './src/Components/MatchMakingOutput.js';
 
 //import reactrouter flux library
 //import additional components
@@ -16,6 +17,7 @@ const RouterComponent = () => {
                 <Scene key="signup" component={SignUp} title="" />
                 <Scene key="userinput" component={UserInput} title="" />
                 <Scene key="useroutlook" component={UserOutlook} title="" />
+                <Scene key="matchmaking" component={MatchMakingOutput} title= ""/>
             </Stack>
         </Router>
     )
